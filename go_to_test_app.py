34f28cb6ec6c88a196c54ff230917b193adb5510
deryann <deryann@gmail.com>
2024-11-17 16:33:34 +0800

    fix: filename ext  part.

--- go_to_test_app.py
+++ go_to_test_app.py
@@ -270,7 +270,7 @@ def load_data_set():
     if CONST_CURRENT_TEST_DATA_SET == "1A":
         idx_useful =(q_start_idx, q_end_idx)
         # idx_reading = (0, -1)
-        with open('1A', 'r') as f:
+        with open('1A.json', 'r') as f:
             dic_words = json.load(f)
         for i in range(idx_useful[0], idx_useful[1]+1):
             _word_list.append(dic_words[str(i)])
